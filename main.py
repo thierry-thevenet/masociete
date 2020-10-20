a5ef0408d2d9e5115f0d890a29b05b4aafe5c09a
thierrythevenet1963 <thierry.thevenet@talao.io>
2020-10-20 15:33:58 +0200

    vers 0.2

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 
 from urllib.parse import urlencode
-
 import requests
 from flask import Flask, redirect, request, render_template_string, session, send_from_directory
+import json
 
 version = '0.1.2'
-
 app = Flask(__name__)
 app.secret_key = 'ùmkljùmkjagzklj'
 
+client_id = 'eID4kHps5w0HFdv3fJ9fuVde'
+client_secret = 'Qe99WEiFmTcSmSHfh6z92mbVbrZOa4CfDT6iyNqRqxfd7mBG'
 
-url_callback = 'http://localhost:3000/callback'
-client_id = '211286433e39cce01db448d80181bdfd005554b19cd51b3fe7943f6b3b86ab6e'
-client_secret = '2791a731e6a59f56b6b4dd0d08c9b1f593b5f3658b9fd731cb24248e2669af4b'
+# local masociete.co
+url_callback = 'http://127.0.0.1:4000/callback'
 
-url_callback = 'http://localhost:3000/callback'
+# Talao as an OAuth2 Identity Provider
+talao_url = 'http://127.0.0.1:3000'
+talao_url_authorize = talao_url + '/api/v1/authorize'
+talao_url_token = talao_url + '/api/v1/oauth/token'
+talao_url_userinfo = talao_url + '/api/v1/user_info'
+talao_url_logout = talao_url + '/api/v1/oauth_logout'
 
-fc_url = 'https://fcp.integ01.dev-franceconnect.fr'
-fc_url_authorize = fc_url + '/api/v1/authorize'
-fc_url_token = fc_url + '/api/v1/token'
-fc_url_userinfo = fc_url + '/api/v1/userinfo'
-fc_url_logout = fc_url + '/api/v1/logout'
-
-@app.route('/france_connect', methods=['GET', 'POST'])
+@app.route('/sign_in', methods=['GET', 'POST'])
 def root():
     if request.method == 'POST':
         data = {
@@ -31,21 +30,23 @@ def root():
             'state': 'test',
             'nonce': 'test',
             'redirect_uri': url_callback,
-            'scope': 'openid identite_pivot test thierry',
+            'scope': 'profile resume birthdate phone email',
         }
-        print('POST envoyé = ', urlencode(data))
-        return redirect(fc_url_authorize + '?' + urlencode(data))
+        print('step 1 : demande d autorisation envoyée ')
+        return redirect(talao_url_authorize + '?' + urlencode(data))
+        #http://127.0.0.1:3000/api/v1/authorize?response_type=code&client_id=eID4kHps5w0HFdv3fJ9fuVde&scope=profile+resume+birthdate+email+phone
 
     else:
         html = """
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-        <form method=post enctype=multipart/form-data>
-            <input type="image" src="/uploads/FCboutons-10.png">
-        </form>
-    </body>
-</html>
+           <!DOCTYPE html>
+           <html lang="en">
+           <body>
+               <form method=post enctype=multipart/form-data>
+                   <h3>Se connecter avec Talao Connect</h3>
+                   <input type="image" src="/uploads/talao2.png">
+               </form>
+            </body>
+            </html>
         """
         return render_template_string(html)
 
@@ -63,9 +64,10 @@ def login() :
     return render_template_string(html)
 
 
+
 @app.route('/callback', methods=['GET', 'POST'])
-def france_connect():
-    print(request.args)
+def talao():
+    print('request reçu dans /callback = ', request.args)
     code = request.args.get('code')
     state = request.args.get('state')
     print('code reçu ', code)
@@ -76,35 +78,47 @@ def france_connect():
         'client_id': client_id,
         'client_secret': client_secret,
         'code': code,
+        'scope' : '' # inutile
     }
-    response = requests.post(fc_url_token, data=data)
+    response = requests.post(talao_url_token, data=data, auth=(client_id, client_secret))
+    print('step 2 : demande de access token envoyée')
+    #curl -u G2eHD21HP8ZVmIPVG0tbtqKH:rp9maPLRQEJ3bviGwTMPXvQdcx8YlqONuVDFZSAqupDdgXb9 -XPOST http://127.0.0.1:3000/api/v1/oauth/token -F grant_type=authorization_code -F code=0sWGCgebZ85Z1gZ0teZqpwRyPXcvBEioP9kaEtzC6PtKr8k0
+
     if response.status_code == 200:
         token_data = response.json()
-        print('avant demande info ', token_data )
-        # demande d'info sur l identite pivot
+        # demande d'info sur le profil
         params = {
-            'schema': 'openid',
+            'schema': 'oauth2',
         }
         headers = {
-            'Authorization': 'Bearer %s' % token_data['access_token']
+            'Authorization': 'Bearer %s' % token_data['access_token'],
         }
         session['id_token'] = token_data.get('id_token')
-        user_info_response = requests.get(fc_url_userinfo, params=params, headers=headers)
+        print('step 3 demande de userinfo envoyée')
+        user_info_response = requests.get(talao_url_userinfo, params=params, headers=headers)
+        #curl -H "Authorization: Bearer ${access_token}" http://127.0.0.1:3000/api/v&/userinfo
+
         html = """
 <!DOCTYPE html>
 <html lang="en">
     <body>
+    <h2>Vous etes maintenant connecté a l'application web de Ma Societe.co </h2>
+    <h3> Data received from Talao.io : </h3>
+    <p>
+    
     {% for key, value in user_info_response.json().items() %}
         <div>{{key}}: {{value}}</div>
     {% endfor %}
+    <br>
         <form method=post action="/logout" enctype=multipart/form-data>
             <button>Logout</button>
         </form>
+    </p>
     </body>
 </html>
         """
         return render_template_string(html, user_info_response=user_info_response)
-    return 'Something failed'
+    return 'User did not accept your access'
 
 
 @app.route('/logout', methods=['POST'])
@@ -116,8 +130,7 @@ def logout():
         'state': 'test',
         'post_logout_redirect_uri': 'http://localhost:3000/logout',
         }
-        print(urlencode(data))
-        return redirect(fc_url_logout + '?' + urlencode(data))
+        return redirect(talao_url_logout + '?' + urlencode(data))
     if request.method == 'POST':
         return 'Game over !!!'
 
@@ -141,5 +154,31 @@ def did_check () :
 		</html>"""
 	return render_template_string(html)
 
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def talao_2():
+    data = {
+        'grant_type': 'client_credentials',
+        'redirect_uri': "",
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'code': "",
+        'scope' : 'identity'
+    }
+    response = requests.post(talao_url_token, data=data, auth=(client_id, client_secret))
+    print('step 2 : demande de token envoyée')
+    #curl -u G2eHD21HP8ZVmIPVG0tbtqKH:rp9maPLRQEJ3bviGwTMPXvQdcx8YlqONuVDFZSAqupDdgXb9 -XPOST http://127.0.0.1:3000/api/v1/oauth/token -F grant_type=authorization_code -F scope=profile -F code=0sWGCgebZ85Z1gZ0teZqpwRyPXcvBEioP9kaEtzC6PtKr8k0
+    if response.status_code == 200 :
+        token_data = response.json()
+        print('step 3 demande envoyée')
+        headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer %s' % token_data['access_token']}
+        data = {'firstname' : 'pierre', 'lastname' : 'Dupont'}
+        response = requests.post('http://127.0.0.1:3000/api/v1/create_identity', data=json.dumps(data), headers=headers)
+        return response.json()
+    return 'status code probleme'
+
+
 if __name__ == '__main__':
     app.run( port=4000, debug =True)
+
