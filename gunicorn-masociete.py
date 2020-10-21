8195edb448cdae8bb4c93f494599b14b199dd1d1
thierrythevenet1963 <thierry.thevenet@talao.io>
2020-10-21 14:53:29 +0200

    vers 0.3

--- gunicorn-masociete.py
+++ gunicorn-masociete.py
@@ -20,3 +20,6 @@ timeout = 3 * 60  # 3 minutes
 keepalive = 5 * 24 * 60 * 60  # 5 days
 capture_output = True
 
+
+# Environment variables
+raw_env = ["MYCHAIN=talaonet", "MYENV=aws", "WEB3_INFURA_PROJECT_ID=f2be8a3bf04d4a528eb416566f7b5ad6"]
--- main.py
+++ main.py
@@ -3,19 +3,33 @@ from urllib.parse import urlencode
 import requests
 from flask import Flask, redirect, request, render_template_string, session, send_from_directory
 import json
+import os
 
-version = '0.1.2'
+# Environment variables set in gunicornconf.py  and transfered to environment.py
+mychain = os.getenv('MYCHAIN')
+myenv = os.getenv('MYENV')
+print('environment variable : ',mychain, myenv)
+
+
+version = '0.3'
 app = Flask(__name__)
 app.secret_key = 'ùmkljùmkjagzklj'
 
-client_id = 'eID4kHps5w0HFdv3fJ9fuVde'
-client_secret = 'Qe99WEiFmTcSmSHfh6z92mbVbrZOa4CfDT6iyNqRqxfd7mBG'
-
-# local masociete.co
-url_callback = 'http://127.0.0.1:4000/callback'
+# environment
+if myenv == 'airbox' :
+    url_callback = 'http://127.0.0.1:4000/callback'
+    talao_url = 'http://127.0.0.1:3000'
+    upload_path = '/home/thierry/Talao/uploads/'
+    client_id = 'HjoZ7fxzimmUJOCRE2fzeQcd'
+    client_secret = 'yPfyFeK2mn955kPOqHdyzmsMZbznjXEokUjyPTUMrrDUR09T'
+else :
+    url_callback = 'http://masociete.co/callback'
+    talao_url = 'https://talao.co'
+    upload_path = '/home/admin/Talao/uploads/'
+    client_id = 'EmiMhjC1gjNVMu7Sek6Hq0Gs'
+    client_secret = '4O9qKnKWVU2dFlmM0eRWvR0SkBT2gndgt0G2o9HDXzKtjqXZ'
 
 # Talao as an OAuth2 Identity Provider
-talao_url = 'http://127.0.0.1:3000'
 talao_url_authorize = talao_url + '/api/v1/authorize'
 talao_url_token = talao_url + '/api/v1/oauth/token'
 talao_url_userinfo = talao_url + '/api/v1/user_info'
@@ -34,7 +48,6 @@ def root():
         }
         print('step 1 : demande d autorisation envoyée ')
         return redirect(talao_url_authorize + '?' + urlencode(data))
-        #http://127.0.0.1:3000/api/v1/authorize?response_type=code&client_id=eID4kHps5w0HFdv3fJ9fuVde&scope=profile+resume+birthdate+email+phone
 
     else:
         html = """
@@ -54,8 +67,8 @@ def root():
 def login() :
     html = """<html lang="en">
 			<body>
-				<h1>Website de MaSociete.co</h1>
-                <h2>Our Decentralized IDentifiers are : </h2>
+				<h1>Website de Ma Société</h1>
+                <h2>Our Decentralized IDentifiers is : </h2>
                 <p>
                     <li><b>did:talao:talaonet:c5C1B070b46138AC3079cD9Bce10010d6e1fCD8D</b></li>
                 </p>
@@ -70,7 +83,7 @@ def talao():
     print('request reçu dans /callback = ', request.args)
     code = request.args.get('code')
     state = request.args.get('state')
-    print('code reçu ', code)
+    print('grant code reçu ', code)
     print('state', state)
     data = {
         'grant_type': 'authorization_code',
@@ -82,10 +95,9 @@ def talao():
     }
     response = requests.post(talao_url_token, data=data, auth=(client_id, client_secret))
     print('step 2 : demande de access token envoyée')
-    #curl -u G2eHD21HP8ZVmIPVG0tbtqKH:rp9maPLRQEJ3bviGwTMPXvQdcx8YlqONuVDFZSAqupDdgXb9 -XPOST http://127.0.0.1:3000/api/v1/oauth/token -F grant_type=authorization_code -F code=0sWGCgebZ85Z1gZ0teZqpwRyPXcvBEioP9kaEtzC6PtKr8k0
-
     if response.status_code == 200:
         token_data = response.json()
+        print('access token reçu = ', token_data['access_token'])
         # demande d'info sur le profil
         params = {
             'schema': 'oauth2',
@@ -96,26 +108,23 @@ def talao():
         session['id_token'] = token_data.get('id_token')
         print('step 3 demande de userinfo envoyée')
         user_info_response = requests.get(talao_url_userinfo, params=params, headers=headers)
-        #curl -H "Authorization: Bearer ${access_token}" http://127.0.0.1:3000/api/v&/userinfo
-
         html = """
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-    <h2>Vous etes maintenant connecté a l'application web de Ma Societe.co </h2>
-    <h3> Data received from Talao.io : </h3>
-    <p>
-    
-    {% for key, value in user_info_response.json().items() %}
+        <!DOCTYPE html>
+        <html lang="en">
+        <body>
+        <h2>Vous etes maintenant connecté a l'application web de Ma Societe.co </h2>
+        <h3> Data received from Talao.io : </h3>
+        <p>
+        {% for key, value in user_info_response.json().items() %}
         <div>{{key}}: {{value}}</div>
-    {% endfor %}
-    <br>
+        {% endfor %}
+        <br>
         <form method=post action="/logout" enctype=multipart/form-data>
             <button>Logout</button>
         </form>
-    </p>
-    </body>
-</html>
+        </p>
+        </body>
+        </html>
         """
         return render_template_string(html, user_info_response=user_info_response)
     return 'User did not accept your access'
@@ -128,16 +137,16 @@ def logout():
         data = {
         'id_token_hint': id_token,
         'state': 'test',
-        'post_logout_redirect_uri': 'http://localhost:3000/logout',
+        'post_logout_redirect_uri': talao_url_logout,
         }
         return redirect(talao_url_logout + '?' + urlencode(data))
     if request.method == 'POST':
         return 'Game over !!!'
 
+# For logo only
 @app.route('/uploads/<filename>')
 def send_file(filename):
-	return send_from_directory('/home/thierry/Talao/uploads/', filename)
-
+	return send_from_directory(upload_path, filename)
 
 # This is the DID proof
 @app.route('/did/', methods=['GET'])
@@ -154,8 +163,7 @@ def did_check () :
 		</html>"""
 	return render_template_string(html)
 
-
-
+# test de client credentials
 @app.route('/test', methods=['GET', 'POST'])
 def talao_2():
     data = {
@@ -164,17 +172,16 @@ def talao_2():
         'client_id': client_id,
         'client_secret': client_secret,
         'code': "",
-        'scope' : 'identity'
+        'scope' : 'create'
     }
     response = requests.post(talao_url_token, data=data, auth=(client_id, client_secret))
     print('step 2 : demande de token envoyée')
-    #curl -u G2eHD21HP8ZVmIPVG0tbtqKH:rp9maPLRQEJ3bviGwTMPXvQdcx8YlqONuVDFZSAqupDdgXb9 -XPOST http://127.0.0.1:3000/api/v1/oauth/token -F grant_type=authorization_code -F scope=profile -F code=0sWGCgebZ85Z1gZ0teZqpwRyPXcvBEioP9kaEtzC6PtKr8k0
     if response.status_code == 200 :
         token_data = response.json()
         print('step 3 demande envoyée')
         headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer %s' % token_data['access_token']}
-        data = {'firstname' : 'pierre', 'lastname' : 'Dupont'}
-        response = requests.post('http://127.0.0.1:3000/api/v1/create_identity', data=json.dumps(data), headers=headers)
+        data = {'firstname' : 'pierre', 'lastname' : 'Dupont', 'email' : 'pierre.dupont@talao.co'}
+        response = requests.post(talao_url + '/api/v1/create', data=json.dumps(data), headers=headers)
         return response.json()
     return 'status code probleme'
 
